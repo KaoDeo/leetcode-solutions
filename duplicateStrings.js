8a9ec58d5e5a3087a9797075d2c0dd2ffc3323a4
Ana Kasrashvili <ana.qasrashvili00@gmail.com>
2022-04-06 16:23:07 +0400

    initial commit

--- duplicateStrings.js
+++ duplicateStrings.js
@@ -1,15 +1,15 @@
 // Create a function that takes a string and returns a new string with duplicates removed
 
 
- const str = 'This is is a test test string';
+//  const str = 'This is is a test test string';
 // 'This is a test string'
 
 
 function removeDuplicates(str){
 
     const arr = str.split('')
-    const set = new set(str) //set method cannot have a duplicates
-    const newString = [...set].join('') //it's just one of the ways to turn set array into string
+    const set = new set(str) 
+    const newString = [...set].join('') 
     return newArray
 }
 
