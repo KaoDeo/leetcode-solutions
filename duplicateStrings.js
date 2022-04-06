59ee3689644fed59c00da220e5b5af0ff5c89a08
Ana Kasrashvili <91371368+KaoDeo@users.noreply.github.com>
2022-04-06 16:47:22 +0400

    docs: updated

--- duplicateStrings.js
+++ duplicateStrings.js
@@ -1,10 +1,3 @@
-// Create a function that takes a string and returns a new string with duplicates removed
-
-
-//  const str = 'This is is a test test string';
-// 'This is a test string'
-
-
 function removeDuplicates(str){
 
     const arr = str.split('')
